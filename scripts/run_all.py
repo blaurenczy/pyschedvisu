e9c53346d7029ef28c32d315cef371d95d36672c
Balazs Laurenczy <blaurenczy@gmail.com>
2019-10-02 18:04:42 +0200

    Added date range

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -13,11 +13,18 @@ from create_report import create_report
 def run_all():
 
     logging.basicConfig(format='%(asctime)s|%(levelname)s| %(message)s', level=logging.INFO)
-    logging.info("Started running SchedVisu workflow ...")
+    logging.info("Started running SchedVisu workflow")
+
+    # get date range
+    start_date = date(2018, 7, 2)
+    end_date = date(2018, 7, 6)
+    n_days = (end_date - start_date).days
+
+    logging.info("Using dates: %s - %s (%d days)".format(start_date, end_date, n_days))
 
     retrieve_DICOMs_from_PACS()
     extract_data_from_DICOMs()
-    create_report()
+    create_report(start_date, end_date)
 
     logging.info("Finished running SchedVisu workflow.")
 
