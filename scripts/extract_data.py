09fa953e3339788405371bc7b41030fd54f3ddad
Balazs Laurenczy <blaurenczy@gmail.com>
2019-11-14 16:05:40 +0100

    Fixed some errors Implemented some data cleaning features Added summary per machine group

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -6,7 +6,6 @@ import re
 import pandas as pd
 from datetime import datetime as dt
 
-
 def extract_transform_and_save_data_from_files(config):
     """
     Extract, transform and save the relevant series for all days specified by the config.
@@ -22,111 +21,101 @@ def extract_transform_and_save_data_from_files(config):
     start_date = dt.strptime(config['main']['start_date'], '%Y-%m-%d')
     end_date = dt.strptime(config['main']['end_date'], '%Y-%m-%d')
     days_range = pd.date_range(start_date, end_date)
-    
+
     # create the variable holding all the series for all days
     df_series = None
-    
+
     # go through the date range day by day
     for day in days_range:
         logging.debug('Processing {}'.format(day.strftime('%Y%m%d')))
-        
+
         # create the path where the input day's data would be stored
         day_str = day.strftime('%Y%m%d')
         day_save_dir_path = os.path.join('data', day.strftime('%Y'), day.strftime('%Y-%m'))
         day_save_file_path = os.path.join(day_save_dir_path, '{}.pkl'.format(day.strftime('%Y-%m-%d')))
-        
+
         # check if the current date has already been retrieved and saved
         if os.path.isfile(day_save_file_path):
             logging.info('Skipping   {}: save file found at "{}", loading data'.format(day_str, day_save_file_path))
-            
+
             # load the data for the current day
             df_series_for_day = pd.read_pickle(day_save_file_path)
             # concatenate the series of the current day into the global DataFrame
             df_series = pd.concat([df_series, df_series_for_day], sort=False)
-            
+
         # if the current date has not already been retrieved and saved
         else:
             logging.info('Processing {}: no save file found at "{}"'.format(day_str, day_save_file_path))
-                    
+
+    """
     # get a summary of what machines are used in which institution names and modality
-    df_groupby = do_series_groupby(config, df_series)
-    logging.debug(df_groupby)
+    df_series, df_count_series, _ = do_series_groupby(config, df_series)
+    logging.info(df_count_series)
+    """
 
-    return df_series, df_groupby
+    return df_series
 
 
-def do_series_groupby(config, df_series):
+def do_series_groupby(config, df_series_input):
     """
     Group series together using a defined set of columns.
     Args:
         config (dict): a dictionary holding all the necessary parameters
         df_series (DataFrame): a pandas DataFrame holding the series
     Returns:
-        df_grouped (DataFrame): a pandas DataFrame holding the grouped series
+        df_series (DataFrame): a pandas DataFrame holding the series, annotated with the machine group info
+        df_count_series (DataFrame): a pandas DataFrame holding the grouped series, counting the number of series
+        df_count_studies (DataFrame): a pandas DataFrame holding the grouped series, counting the number of studies
     """
-    
-    # define the columns to extract
-    groupby_columns = ['Machine Name', 'Institution Name', 'Modality']
-    # define the columns to use for the grouping
-    extract_columns = ['machine', 'Institution Name', 'Study Instance UID', 'Modality']
-  
-    # create groups of machines
-    machine_groups = []
-    # create a DataFrame that regroups all the grouped sub-DataFrames
-    df_grouped = pd.DataFrame()
-    
-    # get a list of machines and display it
-    machines = set(df_series.machine[~df_series.machine.isnull()])
-    logging.debug('Machines:')
-    for machine in machines: logging.debug('  - {}'.format(machine))
-    
-    # go through the list of machines
-    for machine in machines:
-        # get all the study UIDs for this machine
-        study_UIDs = set(df_series[df_series.machine == machine]['Study Instance UID'])
-        # get the rows corresponding to these study UIDs
-        rows_with_same_study_UIDs = df_series['Study Instance UID'].isin(study_UIDs)
-        # extract some columns from these rows and do some renaming
-        sub_df = df_series[rows_with_same_study_UIDs].loc[:, extract_columns]
-        sub_df = sub_df.rename(columns = {'Study Instance UID': 'Number of Series'})
-        sub_df = sub_df.rename(columns = {'machine': 'Machine Name'})
-        
-        # do the grouping using the input columns
-        sub_df_grouped = sub_df.groupby(groupby_columns).count()
-        
-        # create a unique tag for the specific list of machine names of the selected rows
-        machine_group_name = ', '.join(sorted(list(set(sub_df['Machine Name']))))
-        # if the current list of machines already exists, do not repeat and continue
-        if machine_group_name in machine_groups: continue
-        
-        # search the "common name" for this machine group using the config
-        machine_group_common_name = 'undefined'
-        short_machine_group_name = re.sub(r'[ _]', '', machine_group_name.lower())
-        for conf_machine_name in config['machines']:
-            logging.debug('Matching "{}" and "{}" ("{}") ...'
-                .format(short_machine_group_name, config['machines'][conf_machine_name], conf_machine_name))
-            if short_machine_group_name == config['machines'][conf_machine_name]:
-                machine_group_common_name = conf_machine_name
-                logging.debug('Found match between "{}" and "{}" ("{}")'
-                    .format(machine_group_name, config['machines'][conf_machine_name], conf_machine_name))
-                break
-        
-        # append the current machine group name to the list
-        machine_groups.append(machine_group_name)
-        # assign group index to the current machine group
-        sub_df_grouped['Machine Group #'] = len(machine_groups)
-        # assign group name to the current machine group
-        sub_df_grouped['Machine Group Name'] = machine_group_common_name
-        # append the current sub-DataFrame to the global DataFrame
-        df_grouped = pd.concat([df_grouped, sub_df_grouped])
 
-    # display the machine groups
-    logging.debug('Machine groups:')
-    for machine_group in machine_groups: logging.debug('  - {}'.format(machine_group))
-    # sum up the counts
-    df_grouped = df_grouped.groupby(['Machine Group #', 'Machine Group Name', 'Machine Name', 'Institution Name', 'Modality']).sum()
+    # work with a copy that has a "fresh" index
+    df_series = df_series_input.reset_index(drop=True)
     
-    return df_grouped
+    # drop the machine group list column to restart the processing from scratch
+    df_series = df_series.drop(columns = ['Machine Group List', 'Machine Group'], errors = 'ignore')
+
+    # clean up the institution name field
+    df_series['Institution Name'] = df_series['Institution Name'].apply(lambda inst_name: inst_name.replace('  ', ' '))
+
+    # rename the machine column name and "minify" the machine names
+    df_series = df_series.rename(columns = {'machine': 'Machine'})
+    df_series.loc[:, 'machine_short'] = df_series['Machine'].str.lower()
+    df_series.loc[:, 'machine_short'] = df_series['machine_short'].apply(lambda m: re.sub(r'[ _]', '', m))
+
+    # create a machine group name (as a comma-separated list) for each study
+    df_machine_groups = df_series.groupby(['Study Instance UID'])['machine_short']
+    df_machine_groups = df_machine_groups.apply(lambda m: ','.join(sorted(list(set(m)))))
+    df_machine_groups = df_machine_groups.reset_index().rename(
+        columns = {'machine_short': 'Machine Group List'})
+
+    # merge the machine group info back into the series DataFrame
+    df_series = pd.merge(df_machine_groups, df_series, how='inner', on='Study Instance UID')
+
+    # create a DataFrame linking the machine group names to their comma-separated list
+    conf_machine_group = pd.DataFrame(
+        [ [conf_machine, config['machines'][conf_machine]] for conf_machine in config['machines'] ],
+        columns = ['Machine Group', 'Machine Group List'])
+    
+    # merge the machine group name info back into the series DataFrame
+    df_series = pd.merge(df_series, conf_machine_group, how='outer', on='Machine Group List')
+
+    # mark the rows with an undefined machine group
+    df_series.loc[df_series['Machine Group'].isnull(), 'Machine Group'] = 'mixed cases'
+
+    # aggregate series to count the number of series for each sub-group
+    groupby_columns = ['Machine Group', 'Machine Group List', 'Institution Name', 'Machine', 'Modality']
+    df_series_grouped_by_series = df_series.groupby(groupby_columns)
+    df_count_series = pd.DataFrame(df_series_grouped_by_series.count())
+    df_count_series = pd.DataFrame(df_count_series.rename(
+        columns = {'Series Instance UID': 'Number of Series'})['Number of Series'])
+
+    # aggregate series to count the number of studies for each sub-group
+    df_series_grouped_by_study = df_series.groupby(groupby_columns + ['Study Instance UID'])
+    df_count_studies = df_series_grouped_by_study.count().reset_index().groupby(groupby_columns).count()
+    df_count_studies = pd.DataFrame(df_count_studies.rename(
+        columns = {'Study Instance UID': 'Number of Studies'})['Number of Studies'])
+
+    return df_series, df_count_series, df_count_studies
 
 
 def mark_second_takes(config, df_series):
@@ -138,7 +127,7 @@ def mark_second_takes(config, df_series):
     Returns:
         df_series (DataFrame): the pandas DataFrame holding the marked series
     """
-    
+
     # get the list of Study Instance UIDs
     study_UIDs = list(set(df_series['Study Instance UID']))
     logging.info('Found {} unique study UIDs'.format(len(study_UIDs)))
@@ -153,38 +142,38 @@ def mark_second_takes(config, df_series):
     # build the list of rows to keep
     indices_to_keep = []
     for i_study in range(len(study_UIDs)):
-        
+
         # get the series related to the current Study Instance UID
         sUID = study_UIDs[i_study]
         study_str = '[{:2d}/{:2d}] {}...{}'.format(i_study, len(study_UIDs) - 1, sUID[:8], sUID[-5:])
         df_series_for_study = df_series[df_series['Study Instance UID'] == sUID]
         logging.info('Processing  {}: found {:2d} series'.format(study_str, len(df_series_for_study)))
-        
+
         # extract the list of indices for the current study
         indices_for_study = list(df_series_for_study.index.values)
         # sort according to time and keep the last index
         df_last_series_for_study = df_series_for_study.sort_values('end_time')
         # there must be at least 2 series for any splitting
         if len(df_series_for_study) < 2: continue
-        
+
         # check if there is any big difference in the successive series time
         time_diff = df_last_series_for_study['end_time'].apply(lambda t: dt.strptime(t, FMT)).diff()
         time_diff_sec = time_diff[1:].apply(lambda td: td.seconds)
         # get the DataFrame indices where the time difference is bigger than the threshold
         split_indices = list(time_diff_sec[time_diff_sec > study_split_thresh].index)
-        
+
         # if there is no splitting indices
         if len(split_indices) == 0:
             logging.info('  Passing   {}: no second take (max time diff: {:4d} sec.)'.format(study_str, max(time_diff_sec)))
             continue
-        
+
         # if there is more than one split point, throw an error and do not do any splitting
         elif len(split_indices) > 1:
             logging.error('  Error for {}: too many splitting points found: [{}]'
                           .format(study_str, ', '.join(str(split_indices))))
             df_series.loc[indices_for_study, 'i_take'] = None
             continue
-        
+
         # if there is a single splitting time
         else:
             logging.info('  Splitting {}: split between {:3d}/{:3d} [T={}/{}, D={}]'
@@ -197,8 +186,9 @@ def mark_second_takes(config, df_series):
     return df_series
 
 
-def prune_by_time_overlap(df):
+def DEPRECATED_UNUSED_prune_by_time_overlap(df):
     """
+    * DEPRECATED / UNUSED *
     Prune the input DataFrame based on start/end time overlaps.
     Args:
         df (DataFrame): a pandas DataFrame to check for time overlaps
