58e0de50a5db131df06109905681667f5dedf88c
Balazs Laurenczy <blaurenczy@gmail.com>
2019-11-14 11:54:01 +0100

    Creating a standalone configuration loading function

--- scripts/main.py
+++ scripts/main.py
@@ -13,16 +13,21 @@ from scripts.create_report import create_report
 
 
 def run():
+    """
+    Main function running the entire pipeline.
+    Args:
+        None
+    Returns:
+        None
+    """
 
     # create the logger
     logging.basicConfig(format='%(asctime)s|%(funcName)-30.30s:%(lineno)03s|%(levelname)-7s| %(message)s', level=logging.INFO)
 
+    # load the configuration
     logging.info("Reading configuration")
-    # read in the configuration file
-    config = ConfigParser()
-    config.optionxform = str
-    config.read('config.ini')
-    
+    config = load_config()
+
     # set debug level based on the configuration file's content
     logging.getLogger().setLevel(config['main']['debug_level'])
 
@@ -32,6 +37,23 @@ def run():
     extract_transform_and_save_data_from_files(config)
     # create_report(config)
     logging.info("Finished running SchedVisu workflow")
+
+
+def load_config():
+    """
+    Retrieve and save the relevant series from the PACS for all days specified by the config.
+    Args:
+        config (dict): a dictionary holding all the necessary parameters
+    Returns:
+        None
+    """
+
+    # read in the configuration file
+    config = ConfigParser()
+    config.optionxform = str
+    config.read('config.ini')
+
+    return config
 
 
 if __name__ == '__main__':
